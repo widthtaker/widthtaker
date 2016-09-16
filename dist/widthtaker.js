1a12ea6af7533cb4dbc0fc3ad27298813b2da263
bogdanp <bogdan.pestritu@rga.com>
2016-09-16 10:35:09 +0300

    Added padding support and refactored a bit

--- dist/widthtaker.js
+++ dist/widthtaker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/*!
+/**
  * widthtaker.js
  * Version: 0.1.0
  * Copyright 2016 Bogdan Pestritu
@@ -13,30 +13,26 @@
  *  Add `widthtaker` class to the text container whose text you want spaced.
  *  The container should have the target width that you want the text to take.
  *  If you need more control over the target width, use JS.
- *
- *
+ *  Given that the input could be an italicized font it can overflow the container,
+ *  in that case you can add a manual right padding using JS to get it to fit.
  *
  *  __JS__
- *  If you need more control of the width, you can call the `widthtaker` function
- *  with one or multiple arrays with the element you want spaced on the first
- *  position and the target width element on the second:
+ *  If you need more control of the width and padding, you can call the `widthtaker`
+ *  function with one or multiple arrays with the element you want spaced on the first
+ *  position, the target width element on the second, the padding on both sides (px), and the
+ *  right padding for manual fixes (px):
  *
  *  ```
- *  widthtaker([elem, widthEl]);
+ *  widthtaker([elem, widthEl, 20, 5]);
  *  // or
- *  widthtaker([elem1, widthEl1], [elem2, widthEl2],...);
+ *  widthtaker([elem1, widthEl1, 10], [elem2, widthEl2, 0, 5],...);
  *  ```
- *
+ *  The elements passed can be either vanilla JS or jQuery.
  */
 (function (window, document, globalName) {
   'use strict';
 
-  // TODO: resize if font-family changed, first load
-  // TODO: resize if screen resize
   // TODO: bower install
-  // TODO: check if jquery or vanilla elements passed
-  // TODO: don't add negative letter-spacing
-  // TODO: add padding option
 
   var elems = [];
   init();
@@ -71,35 +67,92 @@
   }
 
   function resizeText(elems) {
-    for (var i = 0; i < elems.length; i++) {
-      var contW = elems[i][1].offsetWidth;
-      adjustSpacing(elems[i][0], contW);
-    }
+    elems.map(function (el) {
+      var contW = el[1].offsetWidth,
+          minWidth = getMinWidth(el[0]),
+          padding = el[2] && isNumber(el[2]) ? el[2] : null,
+          paddingRight = el[3] && isNumber(el[3]) ? el[3] : null;
+
+      console.log('minWidth: ', minWidth);
+      adjustSpacing(el[0], contW, minWidth, padding, paddingRight);
+    });
   }
 
-  function adjustSpacing(elem, targetWidth) {
+  function getMinWidth(container) {
+    var minWidth = 0,
+        spanEls = container.children;
+
+    for (var i = 0; i < spanEls.length; i++) {
+      var oldprop = reset(spanEls[i]);
+
+      if (minWidth < spanEls[i].offsetWidth) minWidth = spanEls[i].offsetWidth;
+
+      spanEls[i].style.display = oldprop['display'];
+      spanEls[i].style.letterSpacing = oldprop['letter-spacing'];
+      spanEls[i].style.textIndent = oldprop['text-indent'];
+    }
+    return minWidth;
+  }
+
+  function adjustSpacing(elem, targetWidth, minWidth, padding, paddingRight) {
     var childrenEls = elem.children;
+
+    console.log('padding: ', padding);
+
+    //262 - 60 - 151 = 111 - 60 = 51
+    //262 - 60 - 219 = 262 - 249 = -47
+    //dimensiunea finala - padding de ambele parti - dimensiunea textului fara spacing = spatiu disponibil de impartit intre caractere
 
     for (var i = 0; i < childrenEls.length; i++) {
       var oldProperties = reset(childrenEls[i]),
           elWidth = childrenEls[i].offsetWidth,
           charNo = childrenEls[i].innerHTML.length,
           charWidth = elWidth / charNo,
-          remainingSpace = targetWidth - elWidth,
-          ltrSpacing = remainingSpace / (charNo - 1);
+          remainingSpace = targetWidth - minWidth,
+          // use minWidth for calculations
+      ltrSpacing = 0,
+          txtIndent = void 0;
 
-      // let spanStyles = window.getComputedStyle(spanEls[i]);
+      console.log('remainingSpace first: ', remainingSpace);
 
+      // check for padding on both sides
+      if (padding) {
+        remainingSpace = remainingSpace >= 2 * padding ? remainingSpace - 2 * padding : 0;
+        txtIndent = padding;
+      }
+
+      console.log('remainingSpace after padding: ', remainingSpace);
+      // check for right padding
+      if (paddingRight) remainingSpace = remainingSpace >= paddingRight ? remainingSpace - paddingRight : 0;
+
+      // check for negative letter spacing
+      if (remainingSpace < 0) remainingSpace = 0;
+
+      console.log('remainingSpace after right padding: ', remainingSpace);
+
+      // add the minWidth difference if any
+      if (elWidth < minWidth) {
+        remainingSpace += minWidth - elWidth;
+      }
+
+      console.log('minWidth - elWidth: ', minWidth - elWidth);
+
+      ltrSpacing = remainingSpace / (charNo - 1);
+
+      console.log('el: ', childrenEls[i]);
+      console.log('targetWidth: ', targetWidth);
+      console.log('minWidth: ', minWidth);
       console.log('elWidth: ', elWidth);
       console.log('charNo: ', charNo);
       console.log('charWidth: ', charWidth);
-      console.log('remainingSpace: ', remainingSpace);
+      console.log('remainingSpace after minWidth: ', remainingSpace);
       console.log('ltrSpacing: ', ltrSpacing);
 
-      childrenEls[i].style.color = 'red';
+      childrenEls[i].style.color = 'aliceblue';
+      //childrenEls[i].style.color = 'rebeccapurple';
       childrenEls[i].style.letterSpacing = ltrSpacing + 'px';
-      //spanEls[i].style.textIndent = ltrSpacing + 'px';
-      childrenEls[i].style.display = 'inline';
+      if (txtIndent) childrenEls[i].style.textIndent = txtIndent + 'px';
+      childrenEls[i].style.display = oldProperties.display;
 
       console.log('++++++++++++++++++++++++++++++++++');
     }
@@ -137,8 +190,23 @@
     };
   }
 
+  function convertToJS($elem) {
+    // stackoverflow.com/questions/1853223/check-if-object-is-a-jquery-object
+    if (typeof jQuery === 'function' && ($elem instanceof jQuery || 'jquery' in Object($elem))) {
+      $elem = $elem.get(0);
+    }
+
+    return $elem;
+  }
+
+  function isNumber(n) {
+    return !isNaN(n);
+  }
+
   window[globalName] = function () {
     for (var i = 0; i < arguments.length; i++) {
+      arguments[i][0] = convertToJS(arguments[i][0]);
+      arguments[i][1] = convertToJS(arguments[i][1]);
       elems.push(arguments[i]);
     }
 
--- src/widthtaker.js
+++ src/widthtaker.js
@@ -1,4 +1,4 @@
-/*!
+/**
  * widthtaker.js
  * Version: 0.1.0
  * Copyright 2016 Bogdan Pestritu
@@ -12,30 +12,25 @@
  *  The container should have the target width that you want the text to take.
  *  If you need more control over the target width, use JS.
  *  Given that the input could be an italicized font it can overflow the container,
- *  in that case you can add a manual right padding to get it to fit.
+ *  in that case you can add a manual right padding using JS to get it to fit.
  *
  *  __JS__
- *  If you need more control of the width, you can call the `widthtaker` function
- *  with one or multiple arrays with the element you want spaced on the first
- *  position and the target width element on the second:
+ *  If you need more control of the width and padding, you can call the `widthtaker`
+ *  function with one or multiple arrays with the element you want spaced on the first
+ *  position, the target width element on the second, the padding on both sides (px), and the
+ *  right padding for manual fixes (px):
  *
  *  ```
- *  widthtaker([elem, widthEl]);
+ *  widthtaker([elem, widthEl, 20, 5]);
  *  // or
- *  widthtaker([elem1, widthEl1], [elem2, widthEl2],...);
+ *  widthtaker([elem1, widthEl1, 10], [elem2, widthEl2, 0, 5],...);
  *  ```
- *
+ *  The elements passed can be either vanilla JS or jQuery.
  */
 ((window, document, globalName) => {
   'use strict';
 
-  // TODO: resize if font-family changed, first load
-  // TODO: resize if screen resize
   // TODO: bower install
-  // TODO: check if jquery or vanilla elements passed
-  // TODO: don't add negative letter-spacing
-  // TODO: add padding on both sides option
-  // TODO: add padding correct for italic and similar fonts
 
   var elems = [];
   init();
@@ -70,35 +65,92 @@
   }
 
   function resizeText(elems) {
-    for (let i = 0; i < elems.length; i++) {
-      let contW = elems[i][1].offsetWidth;
-      adjustSpacing(elems[i][0], contW);
-    }
+    elems.map(function(el) {
+      let contW = el[1].offsetWidth,
+          minWidth = getMinWidth(el[0]),
+          padding = el[2] && isNumber(el[2]) ? el[2] : null,
+          paddingRight = el[3] && isNumber(el[3]) ? el[3] : null;
+
+      console.log('minWidth: ', minWidth);
+      adjustSpacing(el[0], contW, minWidth, padding, paddingRight);
+    });
   }
 
-  function adjustSpacing(elem, targetWidth) {
+  function getMinWidth(container) {
+    let minWidth = 0,
+        spanEls = container.children;
+
+    for(let i = 0; i < spanEls.length; i++) {
+      let oldprop = reset(spanEls[i]);
+
+      if(minWidth < spanEls[i].offsetWidth) minWidth = spanEls[i].offsetWidth;
+
+      spanEls[i].style.display = oldprop['display'];
+      spanEls[i].style.letterSpacing = oldprop['letter-spacing'];
+      spanEls[i].style.textIndent = oldprop['text-indent'];
+    }
+    return minWidth;
+  }
+
+  function adjustSpacing(elem, targetWidth, minWidth, padding, paddingRight) {
     let childrenEls = elem.children;
+
+    console.log('padding: ', padding);
+
+    //262 - 60 - 151 = 111 - 60 = 51
+    //262 - 60 - 219 = 262 - 249 = -47
+    //dimensiunea finala - padding de ambele parti - dimensiunea textului fara spacing = spatiu disponibil de impartit intre caractere
 
     for (var i = 0; i < childrenEls.length; i++) {
       let oldProperties = reset(childrenEls[i]),
           elWidth = childrenEls[i].offsetWidth,
           charNo = childrenEls[i].innerHTML.length,
           charWidth = elWidth / charNo,
-          remainingSpace = targetWidth - elWidth,
-          ltrSpacing = remainingSpace / (charNo - 1);
+          remainingSpace = targetWidth - minWidth, // use minWidth for calculations
+          ltrSpacing = 0,
+          txtIndent;
 
-      // let spanStyles = window.getComputedStyle(spanEls[i]);
+      console.log('remainingSpace first: ', remainingSpace);
 
+      // check for padding on both sides
+      if(padding) {
+        remainingSpace = remainingSpace >= 2*padding ? remainingSpace - 2*padding : 0;
+        txtIndent = padding;
+      }
+
+      console.log('remainingSpace after padding: ', remainingSpace);
+      // check for right padding
+      if(paddingRight)
+        remainingSpace = remainingSpace >= paddingRight ? remainingSpace - paddingRight : 0;
+
+      // check for negative letter spacing
+      if(remainingSpace < 0) remainingSpace = 0;
+
+      console.log('remainingSpace after right padding: ', remainingSpace);
+
+      // add the minWidth difference if any
+      if(elWidth < minWidth) {
+        remainingSpace += minWidth - elWidth;
+      }
+
+      console.log('minWidth - elWidth: ', minWidth - elWidth);
+
+      ltrSpacing = remainingSpace / (charNo - 1);
+
+      console.log('el: ', childrenEls[i]);
+      console.log('targetWidth: ', targetWidth);
+      console.log('minWidth: ', minWidth);
       console.log('elWidth: ', elWidth);
       console.log('charNo: ', charNo);
       console.log('charWidth: ', charWidth);
-      console.log('remainingSpace: ', remainingSpace);
+      console.log('remainingSpace after minWidth: ', remainingSpace);
       console.log('ltrSpacing: ', ltrSpacing);
 
-      childrenEls[i].style.color = 'red';
+      childrenEls[i].style.color = 'aliceblue';
+      //childrenEls[i].style.color = 'rebeccapurple';
       childrenEls[i].style.letterSpacing = ltrSpacing + 'px';
-      //spanEls[i].style.textIndent = ltrSpacing + 'px';
-      childrenEls[i].style.display = 'inline';
+      if(txtIndent) childrenEls[i].style.textIndent = txtIndent + 'px';
+      childrenEls[i].style.display = oldProperties.display;
 
       console.log('++++++++++++++++++++++++++++++++++');
     }
@@ -135,8 +187,23 @@
     };
 	}
 
+  function convertToJS($elem) {
+    // stackoverflow.com/questions/1853223/check-if-object-is-a-jquery-object
+    if(typeof jQuery === 'function' && ($elem instanceof jQuery || 'jquery' in Object($elem))) {
+      $elem = $elem.get(0);
+    }
+
+    return $elem;
+  }
+
+  function isNumber(n) {
+    return !isNaN(n);
+  }
+
   window[globalName] = function() {
     for (let i = 0; i < arguments.length; i++) {
+      arguments[i][0] = convertToJS(arguments[i][0]);
+      arguments[i][1] = convertToJS(arguments[i][1]);
       elems.push(arguments[i]);
     }
 
